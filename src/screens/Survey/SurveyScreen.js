7a27083d9d09749256a90fe07d1a8198f302de17
Harsh Darji <harshdarji1995@gmail.com>
2022-05-08 22:51:48 +0530

    survey list screen design

--- src/screens/Survey/SurveyScreen.js
+++ src/screens/Survey/SurveyScreen.js
@@ -1,10 +1,48 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, FlatList} from 'react-native';
+import {layout, screens} from '~constants/constants';
+import {Card, Title, Paragraph} from 'react-native-paper';
 
-export default function SurveyScreen() {
+const data = [
+  {
+    title: 'survey #1',
+    address: 'Science City, Thaltej, Ahmedabad, 380060',
+  },
+  {
+    title: 'survey #1',
+    address: 'Makarba, Ahmedabad South, 380051',
+  },
+];
+const SurveyScreen = props => {
+  const {navigation, route} = props;
+
   return (
-    <View>
-      <Text>Survey List</Text>
+    <View style={layout.container}>
+      <FlatList
+        contentContainerStyle={{padding: 12}}
+        data={data}
+        renderItem={({item}) => {
+          return (
+            <Card style={{marginBottom: 12}}>
+              <Card.Content>
+                <Title>{item.title}</Title>
+                <Paragraph>{item.address}</Paragraph>
+              </Card.Content>
+            </Card>
+          );
+        }}
+        ListFooterComponent={
+          <Card
+            style={{marginBottom: 40}}
+            onPress={() => navigation.navigate(screens.surveyDetails)}>
+            <Card.Content>
+              <Title style={{textAlign: 'center'}}>+ Add Survey</Title>
+            </Card.Content>
+          </Card>
+        }
+      />
     </View>
   );
-}
+};
+
+export default SurveyScreen;
