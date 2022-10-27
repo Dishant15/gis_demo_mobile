7199daf631f47187cdcd41c73e50c7f9ad33147d
Harsh Darji <harshdarji1995@gmail.com>
2022-10-27 18:39:00 +0530

    build and bundle name changes for android

--- android/app/src/main/java/com/networkgis/MainActivity.java
+++ android/app/src/main/java/com/networkgis/MainActivity.java
@@ -1,4 +1,4 @@
-package com.netgis.chavda;
+package in.gpstek.netgis;
 import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; // here
--- android/app/src/main/java/com/networkgis/MainApplication.java
+++ android/app/src/main/java/com/networkgis/MainApplication.java
@@ -1,4 +1,4 @@
-package com.netgis.chavda;
+package in.gpstek.netgis;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.netgis.chavda.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("in.gpstek.netgis.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
