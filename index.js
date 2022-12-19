08329168fe9a391e5b4fdca24856bd8fb1eab598
Harsh Darji <harshdarji1995@gmail.com>
2022-12-19 18:30:02 +0530

    tile url testing

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './src/App/App';
+// import App from './src/App/App';
+import App from './src/Common/TestPage';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
